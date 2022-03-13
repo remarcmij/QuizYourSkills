a8a47d0566b1e6bff3f5f85fbb279e6c8598bb31
Jim Cramer <remarcmij@gmail.com>
2022-03-13 19:22:20 +0100

    Remove unneeded file

--- src/helpers/quizTimer.js
+++
@@ -1,9 +0,0 @@
-function quizTimer(elem) {
-  let secs = 0;
-  const pad = (val) => (val > 9 ? val : '0' + val);
-  return setInterval(() => {
-    elem.textContent = `${pad(parseInt(secs / 60, 10))}:${pad(++secs % 60)}`;
-  }, 1000);
-}
-
-export default quizTimer;
